98a2faa965bed7af62bc72952ced42760adcecc8
Mitchell Kouiyoth <110148377+mitch-kou45@users.noreply.github.com>
2025-07-02 20:21:00 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { LoadingScreen } from './components/sections/LoadingScreen'
+
+function App() {
+  return (
+    <>
+     <LoadingScreen />
+    </>
+  )
+}
+
+export default App
