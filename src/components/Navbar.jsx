499b69fcf5f5e700f1d859ce7a89a9208401f5f1
Mitchell Kouiyoth <110148377+mitch-kou45@users.noreply.github.com>
2025-07-07 15:06:52 -0700

    finished navbar for mobile and desktop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,10 @@
+import { useEffect } from "react";
+export const Navbar = ({menuOpen,setMenuOpen}) => {
 
-export const Navbar = () => {
+    useEffect(() => {
+        document.body.style.overflow = menuOpen ?  "hidden" : "";
+    }, [menuOpen]);
+
     return ( 
     <nav className="fixed top-0 w-full z-40 bg-[rbga(10,10,10, 0.8)] backdrop-blur-lg border-b border-white shadow-lg">
         <div className="max-w-5xl mx-auto px-4">
@@ -8,8 +13,38 @@ export const Navbar = () => {
                     {" "}
                     Mitch<span className="text-purple-500">.Kou</span>{" "}
                     </a>
-            </div>
+                    <div className="w-7 h05 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) =>! prev)}
+                        >
+                        &#9776; {/* Hamburger icon */}
+                    </div>
 
+                    <div className="hidden md:flex items-center space-x-4">
+                    <a 
+                        href="#home" 
+                        className="text-gray-300 hove:text transition-colors"
+                    > 
+                    Home
+                    </a>
+                    <a 
+                            href="#about" 
+                            className="text-gray-300 hove:text transition-colors"
+                        > 
+                        About
+                    </a>
+                    <a 
+                            href="#projects" 
+                            className="text-gray-300 hove:text transition-colors"
+                        > 
+                        Projects
+                    </a>
+                    <a 
+                            href="#contact" 
+                            className="text-gray-300 hove:text transition-colors"
+                        > 
+                        Contact
+                    </a>
+                </div>
+            </div>
         </div>
     </nav>
     );
